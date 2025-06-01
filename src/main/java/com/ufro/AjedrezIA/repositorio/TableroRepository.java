886e6eecb411f1ec5dac607cce13e64358d3332b
joseCalfueque <j.calfueque02@ufromail.cl>
2025-06-01 10:43:54 -0400

    clase del package repository

---
+++ src/main/java/com/ufro/AjedrezIA/repositorio/TableroRepository.java
@@ -0,0 +1,11 @@
+package com.ufro.AjedrezIA.repositorio;
+
+import com.ufro.AjedrezIA.entidad.Tablero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableroRepository extends JpaRepository<Tablero, Long> {
+
+
+}
+
+
