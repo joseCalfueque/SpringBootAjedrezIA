8b714088d12cc8d3b1eab89d05dd39e523725a36
joseCalfueque <j.calfueque02@ufromail.cl>
2025-06-01 10:42:20 -0400

    clase default

---
+++ src/test/java/com/ufro/AjedrezIA/AjedrezIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufro.AjedrezIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjedrezIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
