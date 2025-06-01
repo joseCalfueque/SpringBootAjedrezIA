70149c3020e6165f1c490983d9b88edfad89fa0f
joseCalfueque <j.calfueque02@ufromail.cl>
2025-06-01 10:45:32 -0400

    clase default

---
+++ src/main/java/com/ufro/AjedrezIA/AjedrezIaApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.AjedrezIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjedrezIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjedrezIaApplication.class, args);
+	}
+
+}
